6f464ef7dfbfda1971a65825f20fe4161311783d
Nickolay <eninqwert@gmail.com>
2023-11-09 19:53:18 +0300

    hw10 fix code

--- src/main/java/ru/enin/basic/lesson10/Box.java
+++ src/main/java/ru/enin/basic/lesson10/Box.java
@@ -3,7 +3,7 @@ package ru.enin.basic.lesson10;
 public class Box {
     private String color;
     private String item;
-    private int sizeBox;
+    private int size;
     private boolean isOpened;
     private int sizeItem;
 
@@ -11,9 +11,9 @@ public class Box {
         this.color = color;
     }
 
-    public Box(String color, int sizeBox, boolean isOpened) {
+    public Box(String color, int size, boolean isOpened) {
         this.color = color;
-        this.sizeBox = sizeBox;
+        this.size = size;
         this.isOpened = isOpened;
     }
 
@@ -32,7 +32,7 @@ public class Box {
             System.out.println("в коробке лежит предмет");
             return;
         }
-        if (sizeItem > this.sizeBox) {
+        if (sizeItem > this.size) {
             System.out.println("предмет слишком большой");
             return;
         }
@@ -54,7 +54,7 @@ public class Box {
 
     }
     public void info(){
-        System.out.println("цвет коробки: " + color + " " + "размер коробки: " + sizeBox);
+        System.out.println("цвет коробки: " + color + " " + "размер коробки: " + size);
         if (isOpened){
             System.out.println("коробка открыта");
         } else {
